0095ac8aa8259d4cac6a9c6733cfd9ad9241e687
redianmf <redianmf@gmail.com>
2022-10-26 22:32:52 +0700

    FEAT(Octokit): add octokit for fetch data

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    GITHUB_TOKEN: "#PERSONAL_ACCESS_TOKEN",
+    GITHUB_USERNAME: "redianmf",
+  },
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,30 @@
+import { useState, useEffect } from "react";
 import { Container, Row, Col, Table } from "react-bootstrap";
+import { Octokit } from "@octokit/core";
 
 export default function Home() {
+  const [repos, setRepos] = useState("");
+
+  const getRepos = async () => {
+    try {
+      const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
+
+      console.log(process.env.GITHUB_USERNAME);
+      const response = await octokit.request(
+        `GET /users/${process.env.GITHUB_USERNAME}/repos`,
+        {}
+      );
+
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getRepos();
+  }, []);
+
   return (
     <Container fluid>
       <Row>
@@ -13,14 +37,18 @@ export default function Home() {
             <Row>
               <Col className="ms-5 me-5 mt-3">
                 <Table className="table-repos" hover>
+                  <thead>
+                    <tr>
+                      <th>No</th>
+                      <th>Repository Name</th>
+                      <th>Last Updated</th>
+                    </tr>
+                  </thead>
                   <tbody>
                     <tr>
                       <td>1</td>
                       <td>Mark</td>
-                    </tr>
-                    <tr>
-                      <td>2</td>
-                      <td>Jacob</td>
+                      <td>Mark</td>
                     </tr>
                   </tbody>
                 </Table>
