97820187ea383a856fa570d3149320df81b16c4f
redianmf <redianmf@gmail.com>
2022-10-26 21:56:23 +0700

    FEAT(Card): add component and styling

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,35 @@
-import { Container } from "react-bootstrap";
+import { Container, Row, Col, Table } from "react-bootstrap";
 
 export default function Home() {
   return (
     <Container fluid>
-      <div>Hello world</div>
+      <Row>
+        <Col xs={12} md={3}></Col>
+        <Col xs={10} md={6}>
+          <div className="main-card mt-4">
+            <Row>
+              <h1 className="text-center text-white pt-3">My Repos</h1>
+            </Row>
+            <Row>
+              <Col className="ms-5 me-5 mt-3">
+                <Table className="table-repos" hover>
+                  <tbody>
+                    <tr>
+                      <td>1</td>
+                      <td>Mark</td>
+                    </tr>
+                    <tr>
+                      <td>2</td>
+                      <td>Jacob</td>
+                    </tr>
+                  </tbody>
+                </Table>
+              </Col>
+            </Row>
+          </div>
+        </Col>
+        <Col xs={12} md={3}></Col>
+      </Row>
     </Container>
   );
 }
